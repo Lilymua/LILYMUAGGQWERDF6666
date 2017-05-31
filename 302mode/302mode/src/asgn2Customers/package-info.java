721ca971428d93e66075059d38eac88869dc9e32
SKY-20170119UGV\Administrator <wangziyang688@gmail.com>
2017-05-31 21:19:05 +1000

    Add assignment2 Customerfile

--- 302mode/302mode/src/asgn2Customers/package-info.java
+++ 302mode/302mode/src/asgn2Customers/package-info.java
@@ -1,9 +1,12 @@
 /**
-*  This package contains classes that represent customers at the restaurant as well as a class that creates those classes using the factory method pattern. 
-*  There are three types of customers (described in Section 5.2 in the Assignment Specification) represented by three concrete classes 
-*  which inherit from a single abstract Customer class. This package is detailed in Section 7.1 of the Assignment Specification.
-*  
- * @author Person B
+ * This package contains classes that represent customers at the restaurant as
+ * well as a class that creates those classes using the factory method pattern.
+ * There are three types of customers (described in Section 5.2 in the
+ * Assignment Specification) represented by three concrete classes which inherit
+ * from a single abstract Customer class. This package is detailed in Section
+ * 7.1 of the Assignment Specification.
+ * 
+ * @author Ziyang Wang
  *
  */
 package asgn2Customers;
