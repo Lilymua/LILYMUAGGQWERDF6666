da64227cf3b3e4e7e1eeaafadd29ed426e0a440d
joshua10808 <joshua10808@gmail.com>
2017-06-01 13:25:18 +1000

    new updated restaurant

---
+++ asgn2Restaurant/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * This package contains classes that act as a bridge between the GUI, the log
+ * files and the classes in the asgn2Customers and the asgn2Pizzas packages.
+ * 
+ * @author Joshua Wong and Ziyang Wang
+ *
+ */
+package asgn2Restaurant;
